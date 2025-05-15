80981a237018ba1a0c957328e73a0a8402886f5f
amina-droid <51709019+amina-droid@users.noreply.github.com>
2025-05-15 14:50:03 +0500

    fixed button to up

--- src/components/ButtonToUp/index.tsx
+++ src/components/ButtonToUp/index.tsx
@@ -3,6 +3,7 @@ import { ArrowUp } from "lucide-react";
 import { FC, useEffect, useRef, useState } from "react";
 
 import { Button } from "@/ui";
+import { cn } from "@/utils";
 
 export const ButtonToUp: FC = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -45,24 +46,27 @@ export const ButtonToUp: FC = () => {
   return (
     <Button
       onClick={scrollToTop}
-      className={`
+      className={cn(
+        `
         fixed left-[2vw] bottom-[4vw]
-        w-[calc(var(--main-header-size)+1vh)] h-[calc(var(--main-header-size)+1vh)]
-        rounded-full bg-white shadow-md
+        w-[calc(var(--main-header-size)+1vw)] h-[calc(var(--main-header-size)+1vw)]
+        rounded-full shadow-md
         flex items-center justify-center
         transition-all duration-500 ease-in-out
-        hover:scale-110 hover:bg-gray-100
         md:min-w-[calc(var(--main-header-size)+1vw)]
         md:min-h-[calc(var(--main-header-size)+1vw)]
         max-md:min-w-[calc(var(--main-header-size)+2vw)]
         max-md:min-h-[calc(var(--main-header-size)+2vw)]
         max-md:bottom-[calc(4vw+var(--secondary-header-size))]
         z-50
-        ${isVisible ? "opacity-100 scale-100" : "opacity-0 scale-90 pointer-events-none"}
-      `}
-      variant="link"
+        p-0`,
+        isVisible
+          ? "opacity-100 scale-100"
+          : "opacity-0 scale-90 pointer-events-none",
+      )}
+      variant="outline"
     >
-      <ArrowUp className="w-6 h-6 text-black transition-colors hover:text-yellow-300" />
+      <ArrowUp className="w-1/2 h-1/2 text-black  " />
     </Button>
   );
 };
